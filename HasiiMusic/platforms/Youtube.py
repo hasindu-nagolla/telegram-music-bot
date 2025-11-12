875527528a1f8861bbe0a9c56fd969a0939290b8
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-11-12 09:07:24 +0530

    Fix yt search

--- HasiiMusic/platforms/Youtube.py
+++ HasiiMusic/platforms/Youtube.py
@@ -11,12 +11,12 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from Tune.utils.cookie_handler import COOKIE_PATH
-from Tune.utils.database import is_on_off
-from Tune.utils.downloader import download_audio_concurrent, yt_dlp_download
-from Tune.utils.errors import capture_internal_err
-from Tune.utils.formatters import time_to_seconds
-from Tune.utils.tuning import (
+from HasiiMusic.utils.cookie_handler import COOKIE_PATH
+from HasiiMusic.utils.database import is_on_off
+from HasiiMusic.utils.downloader import download_audio_concurrent, yt_dlp_download
+from HasiiMusic.utils.errors import capture_internal_err
+from HasiiMusic.utils.formatters import time_to_seconds
+from HasiiMusic.utils.tuning import (
     YTDLP_TIMEOUT,
     YOUTUBE_META_MAX,
     YOUTUBE_META_TTL,

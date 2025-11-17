b759462f2d394fbefc62ec8fed0ed6b6184f5ac9
hasindu-nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-11-17 12:22:04 +0530

    Updated all control button emojis

--- HasiiMusic/helpers/_inline.py
+++ HasiiMusic/helpers/_inline.py
@@ -35,15 +35,15 @@ class Inline:
             keyboard.append(
                 [
                     self.ikb(
-                        text="►", callback_data=f"controls resume {chat_id}"),
+                        text="▷", callback_data=f"controls resume {chat_id}"),
                     self.ikb(
-                        text="❚❚", callback_data=f"controls pause {chat_id}"),
+                        text="∣ ∣", callback_data=f"controls pause {chat_id}"),
                     self.ikb(
                         text="⟳", callback_data=f"controls replay {chat_id}"),
                     self.ikb(
-                        text="►►", callback_data=f"controls skip {chat_id}"),
+                        text=">>", callback_data=f"controls skip {chat_id}"),
                     self.ikb(
-                        text="■", callback_data=f"controls stop {chat_id}"),
+                        text="▣", callback_data=f"controls stop {chat_id}"),
                 ]
             )
         return self.ikm(keyboard)
